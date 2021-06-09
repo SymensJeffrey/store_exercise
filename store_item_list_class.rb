c0028916214d3f799e356990a80c8a1247a64b57
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-08 19:41:37 -0500

    change input to hash

--- store_item_list_class.rb
+++ store_item_list_class.rb
@@ -5,25 +5,29 @@
 # ]
 
 class StoreItem
-  def initialize (input_product, input_color, input_price)
-    @product = input_product
-    @color = input_color
-    @price = input_price
+  def initialize (options)
+    @product = options[:product]
+    @color = options[:color]
+    @price = options[:price]
   end
-
-  # def print_item
-    
-  # end
 
   def change_price=(input_price)
     @price = input_price    
   end
 
+  # def product
+  #   @product = product
+  # end
+
 end
 
-item1 = StoreItem.new("apple", "red", 1.00)
+item1 = StoreItem.new(product: "apple", color: "red", price:1.00)
 
-p item1
-item1.change_price=(4.00)
-p "changing price"
-p item1
+# p item1
+# item1.change_price=(4.00)
+# p "changing price"
+# p item1
+
+# p item1
+# p item1.product
+
