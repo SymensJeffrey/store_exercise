e50e27eced27b86023edeed809189edbd4676ede
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-08 19:44:22 -0500

    add item 2

--- store_item_list_class.rb
+++ store_item_list_class.rb
@@ -21,13 +21,23 @@ class StoreItem
 
 end
 
-item1 = StoreItem.new(product: "apple", color: "red", price:1.00)
+item1 = StoreItem.new(
+product: "apple",
+color: "red",
+price:1.00
+)
 
+item2 = StoreItem.new(
+product: "oreos",
+color: "brown",
+price: 4.50
+)
+
+# CHANGE PRICE METHOD TEST
 # p item1
 # item1.change_price=(4.00)
 # p "changing price"
 # p item1
 
-# p item1
-# p item1.product
+p item2 
 
